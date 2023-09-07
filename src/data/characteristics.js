262315c4f7c327cb5afbe720c30274ca800fa795
Eynar Alvarez <eynar.j.alvarez.m@gmail.com>
2023-09-07 00:45:36 -0400

    We take the characteristics card data from data folder.

---
+++ src/data/characteristics.js
@@ -0,0 +1,26 @@
+export const characteristics = [
+    {
+        id: 1,
+        title: 'Servicio Ágil y Profesional',
+        description: 'Nos destacamos por la eficiencia, rapidez y profesionalismo a la hora de trabajar.',
+        img: '/img/eficacia.png'
+    },
+    {
+        id: 2,
+        title: 'Soluciones a Medida',
+        description: 'Ofrecemos productos para cubrir las necesidades de cada rubro.',
+        img: '/img/soluciones.png'
+    },
+    {
+        id: 3,
+        title: 'Importadores Directos',
+        description: 'Somos importadores directos de productos de aceros inoxidables.',
+        img: '/img/importacion.png'
+    },
+    {
+        id: 4,
+        title: 'Gran Alcance Nacional',
+        description: 'Proveemos insumos a clientes de todo el país.',
+        img: '/img/alcance.png'
+    },
+]
